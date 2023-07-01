55c8985942b2057d025e5abc60a60c7fd21897ef
Peterson Umoke <umoke10@hotmail.com>
2023-07-01 17:18:46 +0100

    wip paystack

--- lib/src/paystack_pay_now.dart
+++ lib/src/paystack_pay_now.dart
@@ -167,6 +167,15 @@ class _PaystackPayNowState extends State<PaystackPayNow> {
                     });
                   }
 
+                  if (navigation.url ==
+                      'https://api.escalatetechnologies.net/v1/payment/callback') {
+                    /// Check transaction status before closing the view back to the previous screen.
+                    await _checkTransactionStatus(snapshot.data!.reference)
+                        .then((value) {
+                      return Navigator.of(context).pop();
+                    });
+                  }
+
                   if (navigation.url == 'https://standard.paystack.co/close') {
                     /// Check transaction status before closing the view back to the previous screen.
                     await _checkTransactionStatus(snapshot.data!.reference)
